b9bdb7252a5716b8ae0e9efbfa228d2970afb91c
ozturk <ozturktx@gmail.com>
2019-02-25 11:05:57 -0600

    nothing changed

--- src/test/java/com/brite_erp/runners/PurchasesRunner.java
+++ src/test/java/com/brite_erp/runners/PurchasesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         "html:target/default-cucumber-report"},
         features ="src/test/resources/Purchases/",
         glue = "com/brite_erp/step_definitions"
-        ,tags = "@go"
+        ,tags = "@Run"
 // ,dryRun = true
 
         )
